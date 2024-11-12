d294d014623bbfb4d9898ca5923ea703bfaae3a4
gokul-pv <25975535+gokul-pv@users.noreply.github.com>
2024-11-12 19:39:06 +0100

    Homework 2 q2 solution

---
+++ homeworks/hw2/vae/model_cnn.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+
+class VAE_CNN(nn.Module):
+    def __init__(self, latent_dim=16):
+        super().__init__()
+        self.latent_dim = latent_dim
+        
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+        )
+        
+        self.fc_mu = nn.Linear(4 * 4 * 256, latent_dim)
+        self.fc_var = nn.Linear(4 * 4 * 256, latent_dim)
+        
+        # Decoder
+        self.decoder_input = nn.Linear(latent_dim, 4 * 4 * 128)
+        
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(128, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1),
+            nn.Sigmoid()  # ensure output is between [0,1]
+        )
+        
+    def encode(self, x):
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        mu = self.fc_mu(x)
+        log_var = self.fc_var(x)
+        return mu, log_var
+    
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+    
+    def decode(self, z):
+        x = self.decoder_input(z)
+        x = x.view(x.size(0), 128, 4, 4)
+        x = self.decoder(x)
+        return x
+    
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        return self.decode(z), mu, log_var
+
